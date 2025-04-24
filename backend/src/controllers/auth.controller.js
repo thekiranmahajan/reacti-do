10b41632df59fe8ed97a087576d5a384cc266452
Kiran Mahajan <kiran.mahajan@ansrsource.com>
2025-04-24 15:29:26 +0530

    feat: implement signup functionality with token generation and user validation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,52 @@
-export const signup = () => {};
-export const login = () => {};
-export const logout = () => {};
-export const checkAuth = () => {};
-export const updateProfile = () => {};
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bycrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 6 characters" });
+    }
+    const user = await User.findOne({ email });
+
+    if (user) return res.status(400).json({ message: "Email already exists" });
+
+    const salt = await bycrypt.genSalt(10);
+    const hashedPassword = await bycrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(200).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
+    }
+  } catch (error) {
+    console.log("Error in signup controller: ", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+export const login = async (req, res) => {};
+export const logout = async (req, res) => {};
+export const checkAuth = async (req, res) => {};
+export const updateProfile = async (req, res) => {};
