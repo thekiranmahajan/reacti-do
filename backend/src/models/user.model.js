8036acf29a77cec72b7fb2a6a670f31ef7dcb4b7
Kiran Mahajan <kiran.mahajan@ansrsource.com>
2025-04-24 15:13:48 +0530

    fix: incomplete user model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,23 +1,31 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dyb7cgqfc/image/upload/v1745487507/profile-picture_lu1cgz.svg",
+    },
   },
-  fullName: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6,
-  },
-  profilePic: {
-    type: String,
-    default:
-      "https://res.cloudinary.com/dyb7cgqfc/image/upload/v1745487507/profile-picture_lu1cgz.svg",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
