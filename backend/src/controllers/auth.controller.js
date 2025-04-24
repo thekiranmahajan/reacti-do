8a856be7f7dbc9f02fac9abde16ddd8201619b92
Kiran Mahajan <kiran.mahajan@ansrsource.com>
2025-04-24 15:35:43 +0530

    feat: implement logout functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -77,6 +77,16 @@ export const login = async (req, res) => {
     });
   }
 };
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller: ", error.message);
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
 export const checkAuth = async (req, res) => {};
 export const updateProfile = async (req, res) => {};
